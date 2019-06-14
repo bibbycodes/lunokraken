6e90a68e7b1fcc115e20fe97bcf4b9c4e5819df4
Robert Griffith <robertsomigriffith@gmail.com>
2019-06-14 19:05:47 +0100

    saving before pull

--- routes/api/prices.js
+++ routes/api/prices.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const Price = require('../../models/prices')
 
 router.post('/latest',  (req, res) => {
@@ -94,3 +95,4 @@ router.get('/kraken', (req,res) => {
     })
 
     module.exports = router
+    console.log(getPrices())
